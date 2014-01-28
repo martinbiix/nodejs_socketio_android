ee3f403c3f511c16679b3aa7a3cd4427a9f0447e
Juan Martín Crespo <martin@biixa.com>
2014-01-27 18:39:09 -0600

    Cambio tabulaciones por espacios

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-var express	=require('express'),
-	http	=require('http'),
-	app		=express(),
-	server	=http.createServer(app),
-	puerto	=3000;
+var express =require('express'),
+    http    =require('http'),
+    app     =express(),
+    server  =http.createServer(app),
+    puerto  =3000;
 
-	server.listen(puerto);
-	var io	=require('socket.io').listen(server)
+    server.listen(puerto);
+    var io  =require('socket.io').listen(server)
 
-	io.sockets.on('connection',function(socket){
-		//console.log('Śe detecto un cliente!');
+    io.sockets.on('connection',function(socket){
+        //console.log('Śe detecto un cliente!');
 
-		socket.on('login',function(dato){
-			var response=':D -> '+dato;
-			console.log('login: '+response);
-			io.sockets.emit('response',response);
-		});
+        socket.on('login',function(dato){
+            var response=':D -> '+dato;
+            console.log('login: '+response);
+            io.sockets.emit('response',response);
+        });
 
-		socket.on('logout',function(dato){
-			var respose =':( -> '+dato;
-			console.log('logout: '+response);
-			io.sockets.emit('response',response);
-		});
-	});
+        socket.on('logout',function(dato){
+            var respose =':( -> '+dato;
+            console.log('logout: '+response);
+            io.sockets.emit('response',response);
+        });
+    });
